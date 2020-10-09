053314dd9f0271e13bafa6849400c9c012b6995e
njucz <zhcheng@microsoft.com>
2020-10-09 13:26:44 +0800

    update

--- azurerm/internal/services/kusto/tests/kusto_table_resource_test.go
+++ azurerm/internal/services/kusto/tests/kusto_table_resource_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -28,15 +27,5 @@ func TestAccAzureRMKustoTable_basic(t *testing.T) {
 }
 
 func testAccAzureRMKustoTable_basic(data acceptance.TestData) string {
-	return fmt.Sprintf(`
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_kusto_table" "test" {
-  name                = "table1"
-  kusto_database_id   = "/subscriptions/85b3dbca-5974-4067-9669-67a141095a76/resourceGroups/cwz/providers/Microsoft.Kusto/Clusters/czcz/Databases/a"
-  schema              = "Level:string, Timestamp:datetime, UserId:string, TraceId:string, Message:string, ProcessId:int32"
-}
-`)
+	return ""
 }
